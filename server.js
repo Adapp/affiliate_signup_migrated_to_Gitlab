9fe1dffa96eec8cb7006d96395e12c61c1730a1a
Jack Morgan <jack@hasoffers.com>
2013-04-23 15:24:23 -0700

    Added configurable logging to the server.

--- server.js
+++ server.js
@@ -1,45 +1,88 @@
 var express = require('express'),
   http = require('http'),
   path = require('path'),
-  zipper = require('./zipBuilder');
+  zipper = require('./zipBuilder'),
+  commander = require('commander'),
+  expressWinston = require('express-winston'),
+  winston = require('winston'),
+  fs = require('fs'),
+  q = require('q');
 
 var app = module.exports = express();
 
-app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'assets')));
+commander
+  .version('0.1.0')
+  .option('-c, --config [path]', 'Absolute path to your config file.')
+  .option('-p, --port [port]', 'The port to run the application on.')
+  .parse(process.argv);
+
+var waitForConfig = new q.defer();
+
+if (!commander.config) {
+  app.set('port', commander.port || 3000);
+  waitForConfig.resolve();
+} else{
+  fs.readFile(commander.config, function(err, data) {
+    if (err) {
+      console.error('There was an error reading from ' + command.config + ' ', err);
+      process.exit(1);
+    }
+
+    data = JSON.parse(data);
+    app.set('port', commander.port || data.port || 3000);
+    app.set('logLocation', data.logLocation);
+    app.set('maxLogSize', data.maxLogSize);
+    app.set('maxLogFiles', data.maxLogFiles);
+
+    waitForConfig.resolve();
+  });
+}
+
+waitForConfig.promise.then(function() {
+  app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+
+    if (app.get('logLocation')) {
+      app.use(expressWinston.logger({
+        'transports': [
+          new winston.transports.File({
+            'filename' : path.join(__dirname, app.get('logLocation')),
+            'maxsize'  :  app.get('maxLogSize'),
+            'maxFiles' : app.get('maxLogFiles')
+          })
+        ]
+      }));
+    }
+
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'assets')));
 });
 
-app.configure('development', function() {
-  app.use(express.logger('dev'));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true}));
-  app.set('port', 3000);
-});
-
-app.configure('production', function() {
-  app.set('port', process.env.AFF_SIGNUP_PORT || 8080);
-});
 
 
-app.get('/', function(req, res) {
-  res.render('index');
-});
 
-app.post('/compile', function(req, res) {
-  zipper(req.body, function(archive) {
-    res.setHeader('Content-type', 'application/octet-stream');
-    res.setHeader('Content-disposition', 'attachment; filename=custom_signup.zip');
-    res.send(archive.toBuffer());
-    res.end();
+
+
+  app.get('/', function(req, res) {
+    res.render('index');
   });
 
-});
+  app.post('/compile', function(req, res) {
+    zipper(req.body, function(archive) {
+      res.setHeader('Content-type', 'application/octet-stream');
+      res.setHeader('Content-disposition', 'attachment; filename=custom_signup.zip');
+      res.send(archive.toBuffer());
+      res.end();
+    });
+  });
 
-http.createServer(app).listen(app.get('port'), function() {
-  console.log("Express server listening on port " + app.get('port'));
-});
+  http.createServer(app).listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
+  });
+})
+
+
